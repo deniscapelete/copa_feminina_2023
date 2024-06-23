a8890a5b6be1bd0f115f69ffd3505f32da3289d1
Denis <capelete.denis@hotmail.com>
2024-06-23 10:35:36 -0300

    Criando o Componente Tabela de Jogos

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Card from './components/Card'
+import GameTable from './components/GameTable'
 
 function App() {
 
@@ -8,6 +9,11 @@ function App() {
       <h1>Copa do Mundo Feminina 2023</h1>
       <section className='card'>
         <Card />
+      </section>
+
+      <h2>Tabela de Jogos</h2>
+      <section className='game_table'>
+        <GameTable />
       </section>
     </>
   )
